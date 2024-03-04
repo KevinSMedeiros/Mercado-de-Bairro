77f1d1bdf9e4049fa8ff3d0b04bf2b5dcdbb21c3
Pedro G. Vargas <127370660+pedrovargascba@users.noreply.github.com>
2024-03-04 00:14:12 -0400

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -28,18 +28,25 @@ int quantidadeClientesCSV();
 */
 void exibirCliente(CLIENTE c);
 
+/**
+ * Gravando os dados de um cliente no final do arquivo. Caso o arquivo
+ * não exista, gera um novo arquivo com as colunas que são o cabeçalho
+ * @param c Cliente que será salvo no registro
+*/
 void cadastrarCliente(CLIENTE c);
 
-CLIENTE retornarClienteNaLinha(int indiceDesejado);
+CLIENTE retornarClienteNaLinha(int indiceDesejado); // recebe um inteiro que fala em qual linha buscar,
+//ai ele retorna com um valor de tipo CLIENTE com todas as informacoes na linha
 
+int buscarClientePorCPF(char cpf[15]); //encontra e retorna com o id do cliente no arquivo csv.
 
-void buscarClientePorCPF(char cpf[15]);
+int buscarClientePorNome(char cpf[51]); //encontra e retorna com o id do cliente no arquivo csv.
 
-void buscarClientePorNome(char cpf[51]);
+void mostrarNomesComMaisDeMilPontos(); // gera uma lista de nomes dos clientes com mais de 1000 pontos
 
-void mostrarNomesComMaisDeMilPontos();
+int quantosClientesEntre18e25(); // calcula o numero de clientes entre 18 e 25 anos.
 
-int quantosClientesEntre18e25();
+void modificarCliente(); // funcao que consegue alterar informacoes numa linha no meio do arquivo
 
 
 #endif
